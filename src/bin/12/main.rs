be199d4fb632b1c0ff13d3375b0401c5b96f3f7f
shiku <u0iqf4xm@anonaddy.me>
2023-12-14 09:15:51 +0100

    meh

--- src/bin/12/main.rs
+++ src/bin/12/main.rs
@@ -1,9 +1,29 @@
 #![feature(test)]
+#![feature(ascii_char)]
+
+use std::collections::HashMap;
+use std::hash::Hash;
 
 type Solution = u64;
 pub type ParseOutput = Vec<(Vec<u8>, Vec<u8>)>;
 const MAIN_INPUT: &str = include_str!("main_input");
 const TEST_INPUT: &str = include_str!("test_input");
+pub type Memoization = HashMap<Cell, Solution>;
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Cell {
+    template: Vec<u8>,
+    inserts_left: Vec<u8>,
+}
+
+impl Cell {
+    pub fn to_string(&self) -> String {
+        format!(
+            "[{} - {:?}]",
+            self.template.iter().map(|c| *c as char).collect::<String>(),
+            self.inserts_left
+        )
+    }
+}
 
 pub fn parse(file: &str) -> ParseOutput {
     file.lines()
@@ -19,22 +39,31 @@ pub fn parse(file: &str) -> ParseOutput {
 }
 
 fn part_1(out: &ParseOutput) -> Solution {
+    let mut memoization = HashMap::new();
     out.clone()
         .into_iter()
+        .skip(1)
         .map(|(mut arr, ins)| {
-            let mut sol = 0;
-            solve_rec(&mut arr, ins, 0, &mut sol);
-
+            let sol = solve_rec(arr, ins, 0, &mut memoization);
+            println!(
+                "{}",
+                memoization
+                    .iter()
+                    .map(|(c, s)| format!("{} - {}\n", c.to_string(), s))
+                    .collect::<String>()
+            );
+            memoization.clear();
+            panic!("one");
             sol
         })
         .sum()
 }
 
 fn part_2(out: &mut ParseOutput) -> Solution {
+    let mut memoization = HashMap::new();
     out.clone()
         .into_iter()
         .map(|(mut arr, ins)| {
-            let mut sol = 0;
             let mut arr_2 = Vec::new();
             let mut ins_2 = Vec::new();
             for _ in 0..5 {
@@ -43,18 +72,19 @@ fn part_2(out: &mut ParseOutput) -> Solution {
                 ins_2.extend(ins.clone());
             }
             arr_2.pop();
-            solve_rec(&mut arr_2, ins_2, 0, &mut sol);
+            let mut sol = solve_rec(arr_2, ins_2, 0, &mut memoization);
+            memoization.clear();
             sol
         })
         .sum()
 }
 
 fn solve_rec(
-    current_sol: &mut Vec<u8>,
+    mut current_sol: Vec<u8>,
     mut left_ins: Vec<u8>,
     start_i: usize,
-    sol_count: &mut Solution,
-) {
+    memoization: &mut Memoization,
+) -> Solution {
     fn handle_num(fill: &mut Option<u8>, left_ins: &mut Vec<u8>) -> bool {
         if fill.is_none() {
             *fill = left_ins.pop();
@@ -76,45 +106,116 @@ fn solve_rec(
     for i in start_i..current_sol.len() {
         match current_sol[i] {
             b'?' => {
+                println!("Is ?");
+                println!(
+                    "we are at {:?}",
+                    current_sol.iter().map(|c| *c as char).collect::<String>()
+                );
                 // . case
-                let mut c = current_sol.clone();
-                c[i] = b'.';
+                println!(". case");
                 match &current_fill {
-                    Some(0) | None => solve_rec(&mut c, left_ins.clone(), i + 1, sol_count),
-                    _ => {}
+                    Some(0) | None => {
+                        println!("No current fill so we can proceed");
+                        let cell = Cell {
+                            template: current_sol[i..].to_owned(),
+                            inserts_left: left_ins.clone(),
+                        };
+                        let mut c = current_sol.clone();
+                        c[i] = b'.';
+                        let next_i = match c[i + 1..].iter().position(|c| *c == b'?') {
+                            Some(q_i) => i + 1 + q_i,
+                            None => i + 1,
+                        };
+                        let cell_next = Cell {
+                            template: c[next_i..].to_owned(),
+                            inserts_left: left_ins.clone(),
+                        };
+                        println!(
+                            "going deeper {} {} {}",
+                            cell.to_string(),
+                            cell_next.to_string(),
+                            i
+                        );
+                        *memoization.entry(cell).or_default() += match memoization.get(&cell_next) {
+                            Some(c) => {
+                                println!("Cache hit? {}", c);
+
+                                *c
+                            }
+                            None => {
+                                let w = solve_rec(c.clone(), left_ins.clone(), i + 1, memoization);
+                                println!(
+                                    "back {} {:?} {:?}",
+                                    w,
+                                    current_sol[i..]
+                                        .iter()
+                                        .map(|c| *c as char)
+                                        .collect::<String>(),
+                                    left_ins
+                                );
+                                w
+                            }
+                        };
+                    }
+                    _ => {
+                        println!("Fill active so skip");
+                    }
                 }
+                println!(
+                    "we are at after {:?} {}",
+                    current_sol.iter().map(|c| *c as char).collect::<String>(),
+                    i
+                );
                 // # case
+                println!("# case");
                 if !handle_num(&mut current_fill, &mut left_ins) {
-                    return;
-                } else {
-                    current_sol[i] = b'#';
+                    println!("Break in ? {:?}", left_ins);
+                    return 0;
                 }
+                current_sol[i] = b'#';
+                println!(
+                    "inserted # {:?}",
+                    current_sol.iter().map(|c| *c as char).collect::<String>()
+                );
             }
             b'#' => {
+                println!("Is #");
                 if !handle_num(&mut current_fill, &mut left_ins) {
-                    return;
+                    println!("Break in # {:?}", left_ins);
+                    return 0;
                 }
             }
             b'.' => {
+                println!("Is .");
                 if let Some(c) = current_fill {
                     if c == 0 {
                         // back to no fill
                         current_fill = None;
                     } else {
                         // Break if we are filling up right now and we still have something left to fill
-                        return;
+                        println!("Break in . {:?}", left_ins);
+                        return 0;
                     }
                 }
             }
             d => panic!("{}", d),
         }
     }
-    match (current_fill, left_ins.is_empty()) {
-        (Some(0) | None, true) => {
-            *sol_count += 1;
-        }
-        _ => {}
-    }
+
+    let end = match (current_fill, left_ins.is_empty()) {
+        (Some(0) | None, true) => 1,
+        _ => 0,
+    };
+    println!(
+        "Got to the end {} {} {}",
+        end,
+        current_sol.iter().map(|c| *c as char).collect::<String>(),
+        current_sol[start_i..]
+            .iter()
+            .map(|c| *c as char)
+            .collect::<String>()
+    );
+    end
 }
 fn main() {
     let parse_output = &mut parse(MAIN_INPUT);
@@ -168,81 +269,71 @@ mod tests {
     #[test]
     pub fn test_solve_rec() {
         let mut sol = 0;
+        let mut memoization = HashMap::new();
         solve_rec(
-            &mut vec![b'?', b'?', b'?', b'.', b'#', b'#', b'#'],
+            vec![b'?', b'?', b'?', b'.', b'#', b'#', b'#'],
             vec![3, 1, 1],
             0,
-            &mut sol,
+            &mut memoization,
         );
         assert_eq!(sol, 1, "first");
-
+        memoization.clear();
         let mut sol = 0;
         solve_rec(
-            &mut vec![
+            vec![
                 b'.', b'?', b'?', b'.', b'.', b'?', b'?', b'.', b'.', b'.', b'?', b'#', b'#', b'.',
             ],
             vec![3, 1, 1],
             0,
-            &mut sol,
+            &mut memoization,
         );
         assert_eq!(sol, 4);
-
+        memoization.clear();
         let mut sol = 0;
         solve_rec(
-            &mut vec![
+            vec![
                 b'?', b'#', b'?', b'#', b'?', b'#', b'?', b'#', b'?', b'#', b'?', b'#', b'?', b'#',
                 b'?',
             ],
             vec![6, 1, 3, 1],
             0,
-            &mut sol,
+            &mut memoization,
         );
         assert_eq!(sol, 1);
-
+        memoization.clear();
         let mut sol = 0;
         solve_rec(
-            &mut vec![
+            vec![
                 b'?', b'?', b'?', b'?', b'.', b'#', b'.', b'.', b'.', b'#', b'.', b'.', b'.',
             ],
             vec![1, 1, 4],
             0,
-            &mut sol,
+            &mut memoization,
         );
         assert_eq!(sol, 1);
-
+        memoization.clear();
         let mut sol = 0;
         solve_rec(
-            &mut vec![
+            vec![
                 b'?', b'?', b'?', b'?', b'.', b'#', b'#', b'#', b'#', b'#', b'#', b'.', b'.', b'#',
                 b'#', b'#', b'#', b'#', b'.',
             ],
             vec![5, 6, 1],
             0,
-            &mut sol,
+            &mut memoization,
         );
         assert_eq!(sol, 4);
-
+        memoization.clear();
         let mut sol = 0;
         solve_rec(
-            &mut vec![
+            vec![
                 b'?', b'#', b'#', b'#', b'?', b'?', b'?', b'?', b'?', b'?', b'?', b'?',
             ],
             vec![1, 2, 3],
             0,
-            &mut sol,
+            &mut memoization,
         );
         assert_eq!(sol, 10);
-    }
-
-    #[test]
-    pub fn test_solve_rec_2() {
-        let mut sol = 0;
-        solve_rec(
-            &mut vec![b'?', b'?', b'?', b'?', b'#', b'#', b'#', b'#', b'?', b'?'],
-            vec![1, 4, 1],
-            0,
-            &mut sol,
-        );
-        assert_eq!(sol, 10);
+        memoization.clear();
     }
 }
---
+++ src/bin/14/main.rs
@@ -0,0 +1,252 @@
+#![feature(test)]
+
+use std::collections::HashMap;
+use std::ops::Range;
+
+type Solution = usize;
+pub type ParseOutput = Vec<Vec<u8>>;
+const MAIN_INPUT: &str = include_str!("main_input");
+const TEST_INPUT: &str = include_str!("test_input");
+
+fn part_1(grid: &ParseOutput) -> Solution {
+    let (len_y, len_x) = (grid.len(), grid[0].len());
+    let mut sol = 0;
+    for x in 0..len_x {
+        let mut current_empty_spaces = 0;
+        for y in 0..len_y {
+            let mut current_height = len_y - y;
+            match grid[y][x] {
+                b'.' => current_empty_spaces += 1,
+                b'O' => sol += current_height + current_empty_spaces,
+                b'#' => current_empty_spaces = 0,
+                _ => panic!(),
+            }
+        }
+    }
+    sol
+}
+
+fn part_2(grid: &mut ParseOutput) -> Solution {
+    let mut g = grid.clone();
+    let len = grid.len();
+    let mut grids: HashMap<Vec<Vec<u8>>, usize> = HashMap::new();
+    let cycles = 1000000000;
+    println!("start {} <-", part_1(&g));
+    print_grid(&g);
+    for i in 1..=cycles {
+        println!("Cycle {}", i);
+        roll_rocks_north(&mut g, 0..len, 0..len);
+        roll_rocks_west(&mut g, 0..len, 0..len);
+        roll_rocks_south(&mut g, 0..len, 0..len);
+        roll_rocks_east(&mut g, 0..len, 0..len);
+        if let Some(cycle_start_i) = grids.get(&g) {
+            let cycle_length = (i - cycle_start_i);
+            println!("amount of cycles till the end {}", cycles - i);
+            println!("cycle length {}", cycle_length);
+            println!(
+                "amount of inner cycles till the end {}",
+                (cycles - i) / cycle_length
+            );
+            println!(
+                "amount of inner cycles till the end {}",
+                (cycles - i) % cycle_length
+            );
+            let index = cycle_start_i + ((cycles - i) % cycle_length);
+            println!("{} {}", cycle_start_i, cycle_length);
+            print_grid(&g);
+            g = grids
+                .into_iter()
+                .find_map(|(a, b)| if b == index { Some(a) } else { None })
+                .unwrap();
+            print_grid(&g);
+            println!("{}", calc_north_support(&g, len));
+
+            break;
+        } else {
+            grids.insert(g.clone(), i);
+        }
+    }
+
+    let mut sol = 0;
+    calc_north_support(&g, len)
+}
+
+pub fn calc_north_support(grid: &ParseOutput, len: usize) -> Solution {
+    let mut sol = 0;
+    for x in 0..len {
+        for y in 0..len {
+            if grid[y][x] == b'O' {
+                sol += len - y;
+            }
+        }
+    }
+    sol
+}
+
+pub fn roll_rocks_north(
+    grid: &mut ParseOutput,
+    range_1: Range<Solution>,
+    range_2: Range<Solution>,
+) {
+    let len = grid.len();
+
+    for r1 in range_1 {
+        let mut current_empty_spaces = 0;
+        for r2 in range_2.start..range_2.end {
+            let mut current_height = len - r2;
+            match grid[r2][r1] {
+                b'.' => current_empty_spaces += 1,
+                b'O' => {
+                    /*println!(
+                        "moving from {} to {} {} {}",
+                        r2,
+                        r2 - current_empty_spaces,
+                        current_height,
+                        current_empty_spaces
+                    );*/
+                    grid[r2][r1] = b'.';
+                    grid[r2 - current_empty_spaces][r1] = b'O';
+                }
+                b'#' => current_empty_spaces = 0,
+                _ => panic!(),
+            }
+        }
+        //println!("-----------------");
+    }
+}
+
+pub fn roll_rocks_west(grid: &mut ParseOutput, range_1: Range<Solution>, range_2: Range<Solution>) {
+    let len = grid.len();
+
+    for r1 in range_1 {
+        let mut current_empty_spaces = 0;
+        for r2 in range_2.start..range_2.end {
+            let mut current_height = len - r2;
+            match grid[r1][r2] {
+                b'.' => current_empty_spaces += 1,
+                b'O' => {
+                    grid[r1][r2] = b'.';
+                    grid[r1][r2 - current_empty_spaces] = b'O';
+                }
+                b'#' => current_empty_spaces = 0,
+                _ => panic!(),
+            }
+        }
+    }
+}
+
+pub fn roll_rocks_south(
+    grid: &mut ParseOutput,
+    range_1: Range<Solution>,
+    range_2: Range<Solution>,
+) {
+    let len = grid.len();
+
+    for r1 in range_1 {
+        let mut current_empty_spaces = 0;
+        for r2 in (range_2.start..range_2.end).rev() {
+            let mut current_height = r2;
+            match grid[r2][r1] {
+                b'.' => current_empty_spaces += 1,
+                b'O' => {
+                    grid[r2][r1] = b'.';
+                    grid[r2 + current_empty_spaces][r1] = b'O';
+                }
+                b'#' => current_empty_spaces = 0,
+                _ => panic!(),
+            }
+        }
+    }
+}
+
+pub fn roll_rocks_east(grid: &mut ParseOutput, range_1: Range<Solution>, range_2: Range<Solution>) {
+    let len = grid.len();
+
+    for r1 in range_1 {
+        let mut current_empty_spaces = 0;
+        for r2 in (range_2.start..range_2.end).rev() {
+            let mut current_height = r2;
+            match grid[r1][r2] {
+                b'.' => current_empty_spaces += 1,
+                b'O' => {
+                    grid[r1][r2] = b'.';
+                    grid[r1][r2 + current_empty_spaces] = b'O';
+                }
+                b'#' => current_empty_spaces = 0,
+                _ => panic!(),
+            }
+        }
+    }
+}
+
+pub fn print_grid(grid: &ParseOutput) {
+    println!(
+        "{}",
+        grid.iter()
+            .map(|l| format!("{}\n", l.iter().map(|c| *c as char).collect::<String>()))
+            .collect::<String>()
+    );
+}
+
+pub fn parse(file: &str) -> ParseOutput {
+    let mut grid = Vec::new();
+
+    for l in file.lines().filter(|l| !l.is_empty()) {
+        let mut line = Vec::new();
+        for character in l.as_bytes().iter() {
+            line.push(*character);
+        }
+        grid.push(line);
+    }
+
+    grid
+}
+fn main() {
+    let parse_output = &mut parse(MAIN_INPUT);
+    println!("Solution to part 1 is {}", part_1(parse_output));
+    println!("Solution to part 2 is {}", part_2(parse_output));
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate test;
+
+    use test::{black_box, Bencher};
+
+    use super::*;
+
+    #[test]
+    pub fn test_part_1() {
+        let parse_output = parse(TEST_INPUT);
+        assert_eq!(part_1(&parse_output), 136);
+    }
+
+    #[test]
+    pub fn test_part_2() {
+        let parse_output = &mut parse(TEST_INPUT);
+        assert_eq!(part_2(parse_output), 64);
+    }
+
+    #[bench]
+    fn bench_parse(b: &mut Bencher) {
+        b.iter(|| {
+            let _ = parse(MAIN_INPUT);
+        });
+    }
+
+    #[bench]
+    fn bench_part_1(b: &mut Bencher) {
+        let parse_output = parse(MAIN_INPUT);
+        b.iter(move || {
+            assert_eq!(part_1(black_box(&parse_output)), 105784);
+        });
+    }
+
+    #[bench]
+    fn bench_part_2(b: &mut Bencher) {
+        let parse_output = &mut parse(MAIN_INPUT);
+        b.iter(|| {
+            assert_eq!(part_2(black_box(parse_output)), 1104);
+        });
+    }
+}
