612cc7a8e39d97a125242b0688341d7ce880ad43
shiku <u0iqf4xm@anonaddy.me>
2023-12-25 15:55:54 +0100

    day 21 approximation

--- src/bin/21/main.rs
+++ src/bin/21/main.rs
@@ -1,13 +1,11 @@
 #![feature(test)]
 
-use std::cmp::{min, Ordering};
+use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::ops::Div;
 
 use fnv::{FnvHashMap, FnvHashSet};
-
 type Solution = usize;
-
 type Grid = Vec<Vec<u8>>;
 pub type ParseOutput = (Grid, (usize, usize));
 
@@ -55,27 +53,45 @@ fn part_1((grid, start): &ParseOutput, max_steps: usize) -> Solution {
 }
 
 fn part_2(out: &ParseOutput, max_steps: usize) -> Solution {
-    let mut current_steps: BinaryHeap<FieldI> = BinaryHeap::new();
-    let is_even_c = !(max_steps % 2 == 0) as usize;
-    let max_plot_count_even = part_1(out, out.0.len() + is_even_c + 3);
-    let max_plot_count_uneven = part_1(out, out.0.len() + is_even_c + 2);
-    let remaining_steps = part_1(out, max_steps % out.0.len());
-    let (grid, start) = out;
-    let fields = (max_steps / grid.len()) * 2;
-    let fields_occupied_in_square = (fields * fields) / 2;
-    let guess = (fields_occupied_in_square / 2 * max_plot_count_uneven)
-        + (fields_occupied_in_square / 2 * max_plot_count_even);
-    println!(
-        "grid_length {} fields {} guess {} remainder {}",
-        grid.len(),
-        fields,
-        guess,
-        remaining_steps
+    let half_grid = out.0.len() / 2;
+    let full_grid = out.0.len();
+    let data_point_1 = (solve_i(out, half_grid), half_grid as f64);
+    let data_point_2 = (
+        solve_i(out, half_grid + full_grid * 2),
+        (half_grid + full_grid * 2) as f64,
     );
+    let data_point_3 = (
+        solve_i(out, half_grid + full_grid * 4),
+        (half_grid + full_grid * 4) as f64,
+    );
+    lagrange_interpolate(
+        &[data_point_1, data_point_2, data_point_3],
+        max_steps as f64,
+    ) as Solution
+}
+
+fn lagrange_interpolate(points: &[(f64, f64)], x: f64) -> f64 {
+    let mut result = 0.0;
+
+    for i in 0..points.len() {
+        let mut li = 1.0;
+        for j in 0..points.len() {
+            if i != j {
+                li *= (x - points[j].1) / (points[i].1 - points[j].1);
+            }
+        }
+        result += points[i].0 * li;
+    }
+
+    result
+}
+
+fn solve_i((grid, start): &ParseOutput, max_steps: usize) -> f64 {
+    let is_even_c = !(max_steps % 2 == 0) as usize;
+    let mut current_steps: BinaryHeap<FieldI> = BinaryHeap::new();
     current_steps.push(FieldI(start.0, start.1, 0, 0, 0));
     let mut fields_with_steps: FnvHashMap<(usize, usize, i32, i32), usize> = FnvHashMap::default();
     let mut processed_paths: FnvHashSet<(usize, usize, i32, i32)> = FnvHashSet::default();
-    let r = -4..=4;
     while let Some(FieldI(y, x, i_y, i_x, path_length)) = current_steps.pop() {
         if !processed_paths.insert((y, x, i_y, i_x)) || path_length > max_steps {
             continue;
@@ -87,145 +103,7 @@ fn part_2(out: &ParseOutput, max_steps: usize) -> Solution {
             current_steps.push(FieldI(n_y, n_x, n_i_y, n_i_x, path_length + 1));
         }
     }
-    let mut right_even_tmpl = FnvHashMap::default();
-    let mut right_even_tmpl2 = FnvHashMap::default();
-    let mut right_odd_tmpl = FnvHashMap::default();
-    for ((y, x, i_y, i_x), s) in &fields_with_steps {
-        if *i_y == 0 && *i_x == 1 {
-            right_odd_tmpl.insert((*y, *x, *i_y, *i_x), *s);
-        }
-        if *i_y == 0 && *i_x == 4 {
-            right_even_tmpl.insert((*y, *x, *i_y, *i_x), *s);
-        }
-        if *i_y == 0 && *i_x == 6 {
-            right_even_tmpl2.insert((*y, *x, *i_y, *i_x), *s);
-        }
-    }
-
-    println!("{}", fields_with_steps.len());
-    let f = (fields as i32 / 2);
-    for i_y in -f..=f {
-        for _ in 0..i_y.abs() {
-            print!("   ")
-        }
-        for i_x in -f + i_y.abs() - 1..=f - i_y.abs() + 1 {
-            print!(
-                "{:0>2} ",
-                fields_with_steps
-                    .keys()
-                    .filter(|(_, _, e, r)| *e == i_y && *r == i_x)
-                    .count()
-            );
-        }
-        println!();
-    }
-
-    for ((y, x, i_y, i_x), s) in &right_even_tmpl {
-        println!("{}", right_even_tmpl2.get(&(*y, *x, 0, 6)).unwrap() - *s);
-    }
-    println!(
-        "{} {}",
-        right_even_tmpl.len(),
-        right_even_tmpl
-            .iter()
-            .filter(|((y, x, i_y, i_x), s)| *s + (grid.len() * 2 * 5) < max_steps)
-            .count()
-    );
-    println!(
-        "{} {}",
-        right_odd_tmpl.len(),
-        right_odd_tmpl
-            .iter()
-            .filter(|((y, x, i_y, i_x), s)| *s + (grid.len() * 2 * 5) < max_steps)
-            .count()
-    );
-    panic!();
-    let f = (fields as i32 / 2) + 1;
-    let mut even_diff_state: FnvHashMap<(usize, usize, i32, i32), usize> = FnvHashMap::default();
-    let mut even_universe = None;
-    let mut uneven_diff_state: FnvHashMap<(usize, usize, i32, i32), usize> = FnvHashMap::default();
-    let mut uneven_universe = None;
-    for i_y in -f..=f {
-        for i_x in -f + i_y.abs() - 1..=f - i_y.abs() + 1 {
-            let mut universe_min_s = 0;
-            //   if ((-f..=-f + 2).contains(&i_x) || (f - 2..=f).contains(&i_x)) && i_y == 0 {
-            println!("##");
-            println!(
-                "{:?}, {:?} - {} {}",
-                i_y,
-                i_x,
-                fields_with_steps
-                    .keys()
-                    .filter(|(_, _, e, r)| *e == i_y && *r == i_x)
-                    .count(),
-                (i_y + i_x) % 2 == 0
-            );
-            println!("##");
-            for y in 0..grid.len() {
-                for x in 0..grid.len() {
-                    if let (Some(s)) = fields_with_steps.get(&(y, x, i_y, i_x)) {
-                        print!("{}", s);
-                    } else {
-                        print!(".")
-                    }
-                }
-                println!();
-            }
-        }
-    }
-    'outer: for i_x in 0..=f + 1 {
-        let mut universe_min_s = usize::MAX;
-        for y in 0..grid.len() {
-            for x in 0..grid.len() {
-                if let Some(s) = fields_with_steps.get(&(y, x, 0, i_x)) {
-                    universe_min_s = min(universe_min_s, *s);
-
-                    if universe_min_s >= grid.len() * 2 {
-                        if (i_x) % 2 == 0 {
-                            even_universe = Some((0, i_x));
-                        } else {
-                            uneven_universe = Some((0, i_x));
-                        }
-                    }
-                    if even_universe.is_some() && uneven_universe.is_some() {
-                        break 'outer;
-                    }
-                }
-            }
-        }
-    }
-    let mut another_guess = 0;
-    for i_y in -f..=f {
-        for i_x in -f + i_y.abs()..=f - i_y.abs() {
-            if (i_y + i_x) % 2 == 0 {
-                another_guess += even_diff_state.len();
-            } else {
-                another_guess += uneven_diff_state.len();
-            }
-            println!(
-                "{} {} {} {}",
-                i_y,
-                i_x,
-                (i_y + i_x) % 2 == 0,
-                (i_y.abs() + i_x.abs()) * (grid.len() as i32)
-            );
-        }
-    }
-    println!("yo {:?} {:?}", max_plot_count_even, max_plot_count_uneven);
-    println!(
-        "actual {} diff {} - {:.2}%",
-        fields_with_steps.len(),
-        fields_with_steps.len() as i64 - guess as i64,
-        (fields_with_steps.len() as f64 - guess as f64).div(fields_with_steps.len() as f64) * 100.0
-    );
-    println!(
-        "actual {} diff {} - {:.2}%",
-        fields_with_steps.len(),
-        fields_with_steps.len() as i64 - another_guess as i64,
-        (fields_with_steps.len() as f64 - another_guess as f64).div(fields_with_steps.len() as f64)
-            * 100.0
-    );
-    fields_with_steps.len()
+    fields_with_steps.len() as f64
 }
 
 fn get_neighbours_i(
@@ -339,17 +217,14 @@ mod tests {
     #[test]
     pub fn test_part_2() {
         let parse_output = parse(TEST_INPUT);
-        //assert_eq!(part_2(&parse_output, 1), 2);
-        //assert_eq!(part_2(&parse_output, 2), 4);
-        //assert_eq!(part_2(&parse_output, 3), 6);
-        //assert_eq!(part_2(&parse_output, 5), 13);
-        //assert_eq!(part_2(&parse_output, 6), 16);
-        //assert_eq!(part_2(&parse_output, 10), 50);
-        //assert_eq!(part_2(&parse_output, 50), 1594);
-        assert_eq!(part_2(&parse_output, 110), 6536);
-        //assert_eq!(part_2(&parse_output, 500), 167004);
-        //assert_eq!(part_2(&parse_output, 1000), 668697);
-        //assert_eq!(part_2(&parse_output, 1500), 1505076);
+        assert_eq!(solve_i(&parse_output, 10), 50.0);
+        assert_eq!(solve_i(&parse_output, 50), 1594.0);
+        assert_eq!(part_2(&parse_output, 10), 50);
+        assert_eq!(part_2(&parse_output, 50), 1594);
+        assert_eq!(part_2(&parse_output, 100), 6536);
+        assert_eq!(part_2(&parse_output, 500), 167004);
+        assert_eq!(part_2(&parse_output, 1000), 668697);
+        assert_eq!(part_2(&parse_output, 5000), 16733044);
     }
 
     #[bench]
