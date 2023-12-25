ea56774c6aa1a7548d07ec63112085954240bedd
shiku <u0iqf4xm@anonaddy.me>
2023-12-25 19:49:40 +0100

    bruteforce back in time day 24

--- src/bin/24/main.rs
+++ src/bin/24/main.rs
@@ -1,22 +1,23 @@
 #![feature(test)]
 
-use std::cmp::{max, min};
 use std::f64;
 
-type Solution = usize;
-type Line2D = (f64, f64);
-type VecFormat2D = ((f64, f64), (f64, f64));
-type VecFormat3D = ((f64, f64, f64), (f64, f64, f64));
+use nalgebra::Vector3;
+
+type Solution = f64;
+type Line2D = (Solution, Solution);
+type VecFormat2D = ((Solution, Solution), (Solution, Solution));
+type VecFormat3D = (Vector3<Solution>, Vector3<Solution>);
 pub type ParseOutput = Vec<VecFormat3D>;
 const MAIN_INPUT: &str = include_str!("main_input");
 const TEST_INPUT: &str = include_str!("test_input");
 
-fn part_1(lines: &ParseOutput, area_min: f64, area_max: f64) -> Solution {
+fn part_1(lines: &ParseOutput, area_min: Solution, area_max: Solution) -> Solution {
     let range = area_min..=area_max;
     let mut points = 0;
     let lines2d: Vec<VecFormat2D> = lines
         .iter()
-        .map(|((z, y, x), (v_z, v_y, v_x))| ((*y, *x), (*v_y, *v_x)))
+        .map(|(p, v)| ((p.y, p.x), (v.y, v.x)))
         .collect();
     for l_i_1 in 0..lines2d.len() {
         for l_i_2 in l_i_1..lines2d.len() {
@@ -36,7 +37,7 @@ fn part_1(lines: &ParseOutput, area_min: f64, area_max: f64) -> Solution {
             }
         }
     }
-    points
+    points as Solution
 }
 
 fn collision_happened_in_the_past(
@@ -65,32 +66,34 @@ fn compute_intersection_point((m1, c1): Line2D, (m2, c2): Line2D) -> Option<(f64
     }
 }
 
-fn part_2(lines: &mut ParseOutput) -> Solution {
-    let intersection_points = [(0.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.0, 0.0, 0.0)];
-    let intersection_point_i = 0;
-    let mut minr = (Solution::MAX, Solution::MAX, Solution::MAX);
-    let mut maxr = (Solution::MIN, Solution::MIN, Solution::MIN);
-    let mut avg = (0.0, 0.0, 0.0);
-    for ((z, y, x), (v_z, v_y, v_x)) in lines.iter() {
-        avg = (avg.0 + v_z, avg.1 + v_y, avg.2 + v_x);
-        minr = (
-            min(minr.0, *z as Solution),
-            min(minr.1, *y as Solution),
-            min(minr.2, *x as Solution),
-        );
-        maxr = (
-            max(maxr.0, *z as Solution),
-            max(maxr.1, *y as Solution),
-            max(maxr.2, *x as Solution),
-        );
+fn part_2(projectiles: &mut ParseOutput) -> Solution {
+    let three_projectiles: ParseOutput = projectiles[0..3].into();
+    let search_vel = 300;
+    let mut intersection_point = (0.0, 0.0, 0.0);
+    'outer: for vz in -search_vel..search_vel {
+        for vy in -search_vel..search_vel {
+            for vx in -search_vel..search_vel {
+                let mut try_projectiles = three_projectiles.clone();
+                for (_, v) in try_projectiles.iter_mut() {
+                    v.z += vz as f64;
+                    v.y += vy as f64;
+                    v.x += vx as f64;
+                }
+                if let (Some(i), Some(i2), Some(i3)) = (
+                    intersect_3d(&try_projectiles[0], &try_projectiles[1]),
+                    intersect_3d(&try_projectiles[1], &try_projectiles[2]),
+                    intersect_3d(&try_projectiles[0], &try_projectiles[2]),
+                ) {
+                    if i == i2 && i == i3 {
+                        println!("{:?}", i);
+                        intersection_point = i;
+                        break 'outer;
+                    }
+                }
+            }
+        }
     }
-    avg = (
-        avg.0 / lines.len() as f64,
-        avg.1 / lines.len() as f64,
-        avg.2 / lines.len() as f64,
-    );
-    println!("{:?} \n{:?} \navg{:?}", minr, maxr, avg);
-    0
+    intersection_point.0 + intersection_point.1 + intersection_point.2
 }
 
 pub fn parse(file: &str) -> ParseOutput {
@@ -106,10 +109,32 @@ pub fn parse(file: &str) -> ParseOutput {
                 .split(", ")
                 .map(|p| p.trim_start().parse().unwrap())
                 .collect::<Vec<f64>>();
-            ((pos[2], pos[1], pos[0]), (vel[2], vel[1], vel[0]))
+            (
+                Vector3::new(pos[0], pos[1], pos[2]),
+                Vector3::new(vel[0], vel[1], vel[2]),
+            )
         })
         .collect()
 }
+
+fn intersect_3d(
+    (p1, v1): &VecFormat3D,
+    (p2, v2): &VecFormat3D,
+) -> Option<(Solution, Solution, Solution)> {
+    // cred https://math.stackexchange.com/questions/270767/find-intersection-of-two-3d-lines
+    let g = p2 - p1;
+    let h = v2.cross(&g).magnitude();
+    let k = v2.cross(&v1).magnitude();
+    if h == 0.0 || k == 0.0 {
+        return None;
+    }
+    Some((
+        (p1.z + (v1.z * h / k).round()) as Solution,
+        (p1.y + (v1.y * h / k).round()) as Solution,
+        (p1.x + (v1.x * h / k).round()) as Solution,
+    ))
+}
+
 fn main() {
     let parse_output = &mut parse(MAIN_INPUT);
     println!(
@@ -122,19 +147,21 @@ fn main() {
 #[cfg(test)]
 mod tests {
     extern crate test;
-    use super::*;
+
     use test::{black_box, Bencher};
+
+    use super::*;
 
     #[test]
     pub fn test_part_1() {
         let parse_output = parse(TEST_INPUT);
-        assert_eq!(part_1(&parse_output, 7.0, 27.0), 2);
+        assert_eq!(part_1(&parse_output, 7.0, 27.0), 2.0);
     }
 
     #[test]
     pub fn test_part_2() {
         let parse_output = &mut parse(TEST_INPUT);
-        assert_eq!(part_2(parse_output), 2);
+        assert_eq!(part_2(parse_output), 47.0);
     }
 
     #[bench]
@@ -153,7 +180,7 @@ mod tests {
                     200000000000000.0,
                     400000000000000.0
                 ),
-                16779
+                16779.0
             );
         });
     }
@@ -162,7 +189,7 @@ mod tests {
     fn bench_part_2(b: &mut Bencher) {
         let parse_output = &mut parse(MAIN_INPUT);
         b.iter(|| {
-            assert_eq!(part_2(black_box(parse_output)), 1104);
+            assert_eq!(part_2(black_box(parse_output)), 1104.0);
         });
     }
 }
